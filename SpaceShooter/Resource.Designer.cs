6b125e69bd059a85250c5d4c7a7ad8b0de5f4b3b
Hideo Fukumori <fukumori@acm.org>
2025-04-12 15:35:34 +0900

    Make the added resource visible to the programms

--- SpaceShooter/Resource.Designer.cs
+++ SpaceShooter/Resource.Designer.cs
@@ -3046,11 +3046,20 @@ namespace SpaceShooter {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ADVENTURE {0}.
+        ///   Looks up a localized string similar to START ADVENTURE {0}.
         /// </summary>
         internal static string MenuCampaign {
             get {
                 return ResourceManager.GetString("MenuCampaign", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to CONTINUE ADVENTURE {0}.
+        /// </summary>
+        internal static string MenuCampaignContinue {
+            get {
+                return ResourceManager.GetString("MenuCampaignContinue", resourceCulture);
             }
         }
         
@@ -4174,6 +4183,15 @@ namespace SpaceShooter {
         internal static string MenuResumeGame {
             get {
                 return ResourceManager.GetString("MenuResumeGame", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to RETURN TO MAIN MENU.
+        /// </summary>
+        internal static string MenuReturnToMain {
+            get {
+                return ResourceManager.GetString("MenuReturnToMain", resourceCulture);
             }
         }
         
